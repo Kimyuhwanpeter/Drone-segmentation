abdb095b7fad5d0852e88dfc6e39cceef5ada29e
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2022-06-08 15:22:03 +0900

    Update train_20.py

--- train_20.py
+++ train_20.py
@@ -178,16 +178,7 @@ def categorical_focal_loss(alpha, gamma=2.):
     return categorical_focal_loss_fixed
 
 def tversky_coef(y_true, y_pred, alpha=0.5, const=K.epsilon()):
-    '''
-    Weighted Sørensen Dice coefficient.
-    
-    Input
-    ----------
-        y_true, y_pred: predicted outputs and targets.
-        const: a constant that smooths the loss gradient and reduces numerical instabilities.
-        
-    '''
-    
+
     # get true pos (TP), false neg (FN), false pos (FP).
     true_pos  = tf.reduce_sum(y_true * y_pred)
     false_neg = tf.reduce_sum(y_true * (1-y_pred))
@@ -200,23 +191,6 @@ def tversky_coef(y_true, y_pred, alpha=0.5, const=K.epsilon()):
 
 def focal_tversky(y_true, y_pred, alpha=0.5, gamma=4/3, const=K.epsilon()):
     
-    '''
-    Focal Tversky Loss (FTL)
-    
-    focal_tversky(y_true, y_pred, alpha=0.5, gamma=4/3)
-    
-    ----------
-    Abraham, N. and Khan, N.M., 2019, April. A novel focal tversky loss function with improved 
-    attention u-net for lesion segmentation. In 2019 IEEE 16th International Symposium on Biomedical Imaging 
-    (ISBI 2019) (pp. 683-687). IEEE.
-    
-    ----------
-    Input
-        alpha: tunable parameter within [0, 1]. Alpha handles imbalance classification cases 
-        gamma: tunable parameter within [1, 3].
-        const: a constant that smooths the loss gradient and reduces numerical instabilities.
-        
-    '''
     # tf tensor casting
     y_pred = tf.convert_to_tensor(y_pred)
     y_true = tf.cast(y_true, y_pred.dtype)
